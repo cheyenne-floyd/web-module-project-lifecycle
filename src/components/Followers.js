f82ea4b2d627b136ebee36663b6faf23ff2cca2d
Cheyenne Floyd <cheyennesfloyd@gmail.com>
2021-07-08 02:44:00 -0400

    complete

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -3,22 +3,29 @@ import React, { Component } from 'react'
 
 class Followers extends Component {
     state = {
-        user: null
+        followers: []
     }
 
     componentDidMount() {
         // fetch user data
         axios.get("https://api.github.com/users/cheyenne-floyd/followers")
-        .then((res) => console.log(res))
+        .then((res) => {
+            this.setState({
+                Followers: res.data,
+            });
+        })
         .catch((err) => console.log(err));
     }
     render() {
-        return (
-            <div>
-
-            </div>
-        )
-    }
+        if (this.state.followers.length === 0) return <p> Wheres the follows?!</p>
+        return (<div>
+            {this.state.follower.map((follower) => (
+            <div key={this.state.follower.id} >
+            <h4>{this.state.follower.login}</h4>
+        </div>
+            ))}
+            </div>);
+}
 }
 
 export default Followers;
--- src/components/User.js
+++ src/components/User.js
@@ -1,30 +1,30 @@
-import axios from 'axios';
-import React, { Component } from 'react'
+    import axios from 'axios';
+    import React, { Component } from 'react'
 
-class User extends Component {
-    state = {
-        user: null
+    class User extends Component {
+        state = {
+            user: null
+        }
+
+        componentDidMount() {
+            // fetch user data
+            axios.get("https://api.github.com/users/cheyenne-floyd")
+            .then((res) => {
+                this.setState({
+                    user: res.data,
+                });
+            })
+            .catch((err) => console.log(err));
+        }
+        render() {
+            if (!this.state.user) return <p>Dont rush me. Im loading ok..</p>;
+            return (
+                <div>
+                    <img style={{ width:"250px" }} src={this.state.user.avatar_url} alt={this.state.user.name} />
+                <h3>{this.state.user.name}</h3>
+                </div>
+            )
+        }
     }
 
-    componentDidMount() {
-        // fetch user data
-        axios.get("https://api.github.com/users/cheyenne-floyd")
-        .then((res) => {
-            this.setState({
-                user: res.data,
-            });
-        })
-        .catch((err) => console.log(err));
-    }
-    render() {
-        if (!this.state.user) return <p>Dont rush me. Im loading ok..</p>;
-        return (
-            <div>
-                <img style={{ width:"250px" }} src={this.state.user.avatar_url} alt={this.state.user.name} />
-            <h3>{this.state.user.name}</h3>
-            </div>
-        )
-    }
-}
-
-export default User;
+    export default User;
