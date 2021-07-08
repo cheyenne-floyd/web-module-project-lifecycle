50284823ef27df9e5f58d7ff32edb2430363f31a
Cheyenne Floyd <cheyennesfloyd@gmail.com>
2021-07-08 02:16:53 -0400

    user card complete

--- src/components/User.js
+++ src/components/User.js
@@ -9,13 +9,19 @@ class User extends Component {
     componentDidMount() {
         // fetch user data
         axios.get("https://api.github.com/users/cheyenne-floyd")
-        .then((res) => console.log(res))
+        .then((res) => {
+            this.setState({
+                user: res.data,
+            });
+        })
         .catch((err) => console.log(err));
     }
     render() {
+        if (!this.state.user) return <p>Dont rush me. Im loading ok..</p>;
         return (
             <div>
-
+                <img style={{ width:"250px" }} src={this.state.user.avatar_url} alt={this.state.user.name} />
+            <h3>{this.state.user.name}</h3>
             </div>
         )
     }
